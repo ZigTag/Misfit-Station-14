ss14-harmony/ss14-harmony

fix: health analyzer control

Content.Client/HealthAnalyzer/UI/HealthAnalyzerControl.xaml.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Numerics;
+using Content.Shared._CD.Traits; // Misfit - Add synth prefix to species
 using Content.Shared.Atmos;
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Prototypes;
@@ -78,11 +79,18 @@ public sealed partial class HealthAnalyzerControl : BoxContainer
             : Loc.GetString("health-analyzer-window-entity-unknown-text"));
         NameLabel.SetMessage(name);
 
-        SpeciesLabel.Text =
-            _entityManager.TryGetComponent<HumanoidAppearanceComponent>(target.Value,
-                out var humanoidAppearanceComponent)
-                ? Loc.GetString(_prototypes.Index<SpeciesPrototype>(humanoidAppearanceComponent.Species).Name)
-                : Loc.GetString("health-analyzer-window-entity-unknown-species-text");
+        // Misfit - Change species identifier to have Synthetic prefix
+        var species = _entityManager.TryGetComponent<HumanoidAppearanceComponent>(target.Value,
+            out var humanoidAppearanceComponent)
+            ? Loc.GetString(_prototypes.Index<SpeciesPrototype>(humanoidAppearanceComponent.Species).Name)
+            : Loc.GetString("health-analyzer-window-entity-unknown-species-text");
+
+        var synthSpecies = _entityManager.HasComponent<SynthComponent>(target.Value)
+            ? Loc.GetString("synthetic-component-suffix", ("species", species))
+            : species;
+
+        SpeciesLabel.Text = synthSpecies;
+        // End Misfit
 
         // Basic Diagnostic
 

ZigTag/Misfit-Station-14

Content.Client/HealthAnalyzer/UI/HealthAnalyzerControl.xaml.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Numerics;
+using Content.Shared._CD.Traits; // Misfit - Add synth prefix to species
 using Content.Shared.Atmos;
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Prototypes;
@@ -78,11 +79,18 @@ public sealed partial class HealthAnalyzerControl : BoxContainer
             : Loc.GetString("health-analyzer-window-entity-unknown-text"));
         NameLabel.SetMessage(name);
 
-        SpeciesLabel.Text =
-            _entityManager.TryGetComponent<HumanoidProfileComponent>(target.Value,
+        // Misfit - Change species identifier to have Synthetic prefix
+        var species = _entityManager.TryGetComponent<HumanoidProfileComponent>(target.Value,
                 out var humanoidComponent)
                 ? Loc.GetString(_prototypes.Index(humanoidComponent.Species).Name)
                 : Loc.GetString("health-analyzer-window-entity-unknown-species-text");
+
+        var synthSpecies = _entityManager.HasComponent<SynthComponent>(target.Value)
+            ? Loc.GetString("synthetic-component-suffix", ("species", species))
+            : species;
+
+        SpeciesLabel.Text = synthSpecies;
+        // End Misfit
 
         // Basic Diagnostic
 